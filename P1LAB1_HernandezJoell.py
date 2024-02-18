be27f8418da8cc505b7fe0a3f86d60938e04a7b7
isjmhh <143422892+isjmhh@users.noreply.github.com>
2024-02-18 06:22:50 -0500

    Add files via upload

---
+++ P1LAB1_HernandezJoell.py
@@ -0,0 +1,11 @@
+# Joel Hernandez
+
+ # 02/18/24
+
+ # P1LAB1
+
+# Take input from the user Output the welcome message
+
+first_name = input("Enter your first name: ")
+
+print("Welcome,", first_name + "!")
